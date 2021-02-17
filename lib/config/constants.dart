0c2341ea72eeb539e3b037c4c0f3f2db6cdb4642
dev_koo <kbi960130@naver.com>
2021-02-17 21:31:50 +0900

    불필요한 코드 제거

--- lib/config/constants.dart
+++ lib/config/constants.dart
@@ -3,11 +3,11 @@ import 'package:hellokorean/config/size_config.dart';
 
 const kPrimaryColor = Colors.green;//Color(0xFFFF7643);
 const kPrimaryLightColor = Color(0xFFFFECDF);
-const kPrimaryGradientColor = LinearGradient(
-  begin: Alignment.topLeft,
-  end: Alignment.bottomRight,
-  colors: [Color(0xFFFFA53E), Color(0xFFFF7643)],
-);
+// const kPrimaryGradientColor = LinearGradient(
+//   begin: Alignment.topLeft,
+//   end: Alignment.bottomRight,
+//   colors: [Color(0xFFFFA53E), Color(0xFFFF7643)],
+// );
 const kSecondaryColor = Color(0xFF979797);
 const kTextColor = Color(0xFF757575);
 const kAppBarColor = Colors.red;
